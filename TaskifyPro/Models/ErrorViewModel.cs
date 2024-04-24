5f0b9a15b7c11ce82b8ff6c0cd92a18053305df0
Jon Frederick <JonFred151@gmail.com>
2024-04-24 14:35:42 -0500

    first init

---
+++ TaskifyPro/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TaskifyPro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
